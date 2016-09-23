9102756733e35ffa270990386c76685e6ee65307
Kokororin <i@kotori.love>
2016-09-23 12:50:19 +0800

    add message for Internet Explorer 8

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const config = require('./webpack.config');
 const open = require('open');
 
 new WebpackDevServer(webpack(config), config.devServer)
-.listen(config.port, 'localhost', (err) => {
+.listen(config.port, '0.0.0.0', (err) => {
   if (err) {
     console.log(err);
   }
-  console.log('Listening at localhost:' + config.port);
+  console.log('Listening at 0.0.0.0:' + config.port);
   console.log('Opening your system browser...');
   open('http://localhost:' + config.port + '/webpack-dev-server/');
 });
