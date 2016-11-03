e8927fe3abd7ade2fbf303167297aca3eef0fefb
Kokororin <i@kotori.love>
2016-11-03 14:52:16 +0800

    Update index.html

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,5 @@
 export cachedFetch from './cachedFetch';
 export hashStr from './hashStr';
 export Piwik from './Piwik';
-export preloadImg from './preloadImg';
 export scrollTo from './scrollTo';
 export Storage from './Storage';
