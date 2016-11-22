3e4026f187fbba491dfc731f8ce7e596b2043fcd
Kokororin <i@kotori.love>
2016-11-22 14:14:45 +0800

    fix rhl

--- cfg/base.js
+++ cfg/base.js
@@ -40,7 +40,8 @@ module.exports = {
       sources: `${defaultSettings.srcPath}/sources/`,
       stores: `${defaultSettings.srcPath}/stores/`,
       styles: `${defaultSettings.srcPath}/styles/`,
-      config: `${defaultSettings.srcPath}/config/` + process.env.REACT_WEBPACK_ENV
+      config: `${defaultSettings.srcPath}/config/` + process.env.REACT_WEBPACK_ENV,
+      'react/lib/ReactMount': 'react-dom/lib/ReactMount'
     }
   },
   module: {}
--- cfg/dev.js
+++ cfg/dev.js
@@ -32,7 +32,7 @@ let config = Object.assign({}, baseConfig, {
 // Add needed loaders to the defaults here
 config.module.loaders.push({
   test: /\.(js|jsx)$/,
-  loader: 'babel-loader',
+  loader: 'react-hot!babel-loader',
   include: [].concat(
     config.additionalPaths,
     [path.join(__dirname, '/../src')]
