a08f40e06ba51b611c8a5f23e2f43956db3c45fb
simon3000 <simon3000@163.com>
2018-06-17 17:46:02 +0800

    use a single line here

--- src/locale/format.js
+++ src/locale/format.js
@@ -2,8 +2,6 @@ import { injectIntl, intlShape } from 'react-intl';
 
 const ChildComponent = ({ intl, id, values }) => intl.formatMessage({ id }, values);
 
-ChildComponent.propTypes = {
-  intl: intlShape.isRequired
-};
+ChildComponent.propTypes = { intl: intlShape.isRequired };
 
 export default injectIntl(ChildComponent);
