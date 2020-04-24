16ac4a4d538544fd2ede6ac6644c3eb7b9fed6c2
Meodinger Wang <54493746+Meodinger@users.noreply.github.com>
2020-04-24 21:01:43 +0800

    Simplified Chinese Support

---
+++ src/locale/zh-cn.ts
@@ -0,0 +1,29 @@
+export default {
+  'x rank': '{rank}名',
+  'Redirect to pixiv': '跳转到pixiv',
+  'Add to Bookmarks': '添加到书签栏',
+  Tweet: 'Tweet',
+  LINE: 'LINE',
+  Comments: '评论',
+  'No Comments': '没有评论',
+  'An Error Occurred': '发生了一个错误',
+  'Communication Error Occurred': '发生了网络错误',
+  'pixiv ID or Email Address is Blank': 'pixiv ID或邮箱地址为空',
+  'Password is Blank': '密码为空',
+  'Wait a Moment': '请稍候...',
+  Login: '登录',
+  Logout: '登出',
+  'Email Address / pixiv ID': '邮箱地址/pixiv ID',
+  Password: '密码',
+  Nickname: '昵称',
+  'An error occurred. Check the URL or wait for a while and access again.':
+    '发生了一个错误，请检查链接或稍等一会儿再访问',
+  'Failed to Load': '加载失败',
+  Tags: '标签',
+  'Redirecting to pixiv.net': '正在跳转到pixiv.net',
+  Language: '语言',
+  Ranking: '排名',
+  HelpToTranslate: '帮助我们翻译',
+  'This page is not available in your area':
+    '这个页面不适用于您的国家或地区'
+};
