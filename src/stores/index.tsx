2ab2ec35bd1ec81f028661b459287c80bb7bc447
kokororin <i@kotori.love>
2020-04-26 14:30:07 +0800

    feat: combine providers

--- src/stores/index.tsx
+++ src/stores/index.tsx
@@ -1,6 +1,5 @@
 import { createBrowserHistory } from 'history';
-import React from 'react';
-
+import { combineProviders } from 'react-combine-provider';
 import { AuthProvider } from '@/stores/AuthStore';
 import { GalleryProvider } from '@/stores/GalleryStore';
 import { IllustProvider } from '@/stores/IllustStore';
@@ -10,18 +9,10 @@ import { SiteProvider } from '@/stores/SiteStore';
 // Create a history of your choosing (we're using a browser history in this case)
 export const history = createBrowserHistory();
 
-export const StoreProvider: React.FunctionComponent<{}> = props => {
-  return (
-    <>
-      <SiteProvider>
-        <LocaleProvider>
-          <AuthProvider>
-            <GalleryProvider>
-              <IllustProvider>{props.children}</IllustProvider>
-            </GalleryProvider>
-          </AuthProvider>
-        </LocaleProvider>
-      </SiteProvider>
-    </>
-  );
-};
+export const StoreProvider = combineProviders([
+  AuthProvider,
+  GalleryProvider,
+  IllustProvider,
+  LocaleProvider,
+  SiteProvider
+]);
